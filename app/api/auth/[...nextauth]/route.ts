f7b803c728b629844dedae79ff95c9ad9ef2324b
vikramsingh13 <vikramsingh.webdev@gmail.com>
2024-09-28 00:14:05 -0400

    setup the next auth api structure with fixed imports and exports

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,11 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth/next";
 // So far we will be using google and github for our auth.
-import GoogleProvider from 'next-auth/providers/google';
-import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
 
 // All requests to /api/auth/* (signIn, callback, signOut, etc.) will automatically be handled by NextAuth.js.
 // authOptions will be used as a param for NextAuth and exported.
-export const authOptions = {
+const authOptions = {
   // Configure the google and github provider ids and secrets.
   providers: [
     GithubProvider({
@@ -15,11 +15,12 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-    })
-  ]
-}
+    }),
+  ],
+};
 
-// Wrap the authOptions configured above with NextAuth to be used for auth use cases. 
-export default NextAuth(authOptions);
+// Wrap the authOptions configured above with NextAuth to be used for auth use cases.
+const handler = NextAuth(authOptions);
 
-
+// Export the Route handler to handle REST-like requests in App Router.
+export { handler as GET, handler as POST };
