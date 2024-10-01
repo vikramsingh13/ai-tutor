2b0bdecde2c59405d8d36b447b849faf86f895c7
vikramsingh13 <vikramsingh.webdev@gmail.com>
2024-10-01 18:28:07 -0400

    auth options refactored into lib/authOptions. It will be used for NextAuth and session management

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,6 @@
 import NextAuth from "next-auth/next";
-// So far we will be using google and github for our auth.
-import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
-
-// All requests to /api/auth/* (signIn, callback, signOut, etc.) will automatically be handled by NextAuth.js.
-// authOptions will be used as a param for NextAuth and exported.
-const authOptions = {
-  // Configure the google and github provider ids and secrets.
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-  ],
-};
+// import the auth options from the lib to be used by the NextAuth.
+import { authOptions } from "@/lib/authOptions";
 
 // Wrap the authOptions configured above with NextAuth to be used for auth use cases.
 const handler = NextAuth(authOptions);
---
+++ lib/authOptions.ts
@@ -0,0 +1,19 @@
+// So far we will be using google and github for our auth.
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+// All requests to /api/auth/* (signIn, callback, signOut, etc.) will automatically be handled by NextAuth.js.
+// authOptions will be used as a param for NextAuth and session management.
+export const authOptions = {
+  // Configure the google and github provider ids and secrets.
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+};
