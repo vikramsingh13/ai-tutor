ecf1ad271fda527ea27d854dc5d3ef7812389ead
vikramsingh13 <vikramsingh.webdev@gmail.com>
2024-10-01 18:04:32 -0400

    oauth buttons added in login page that redirects to the oauth options

--- app/login/[loginType]/page.tsx
+++ app/login/[loginType]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 // Import navigation hooks to help with redirection and getting query params.
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 // Import the login types to decide whether to render signin or signup forms.
 import { UserLoginType } from "@/types/types";
 // Import Zod to help with validation and form schema.
@@ -28,6 +28,8 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 // Login component will render the sign in and sign up pages.
 const Login = () => {
+  // Initialize the router from next/navigation.
+  const router = useRouter();
   // Defines the form using useForm hook.
   const form = useForm();
 
@@ -59,138 +61,160 @@ const Login = () => {
     console.log("login/[loginType]/page: User tried to sign up!");
   };
 
+  // Handles OAuth button click.
+  const handleOAuthButtonClick = () => {
+    // Go to the api OAuth sign in page.
+    router.push(`/api/auth/signin`);
+  };
+
   // Get the login type from the query params
   const { loginTypeFromParams } = useParams();
   // Use state to track whether the user wants to sign in or sign up.
   const [loginType, setLoginType] = useState(loginTypeFromParams);
   return (
-    <div className="flex flex-grow justify-center text-center items-center w-full min-h-screen">
-      <Tabs defaultValue={UserLoginType.SIGN_IN} className="w-[400px]">
-        {/* List of tabs we have. Use grid to style the tab buttons properly.*/}
-        <TabsList className="grid w-full grid-cols-2">
-          {/* Trigger values to display the respective TabsContent. */}
-          <TabsTrigger value={UserLoginType.SIGN_IN}>Sign In</TabsTrigger>
-          <TabsTrigger value={UserLoginType.SIGN_UP}>Sign Up</TabsTrigger>
-        </TabsList>
-        <TabsContent value={UserLoginType.SIGN_IN} className="py-4">
-          {/* Use shadcn Form provider component. */}
-          {/* LoginForm is a useForm() object */}
-          <Form {...LoginForm}>
-            {/* Nest the HTML form tag inside the Form provider */}
-            {/* Pass the submit handler function to LoginForm.handleSubmit in the onSubmit prop.*/}
-            {/* LoginForm.handleSubmit will take care of event handling,  prevent default, etc. */}
-            <form
-              onSubmit={LoginForm.handleSubmit(handleUserSignIn)}
-              className=""
-            >
-              {/* Each FormField will have atleast the control, name and render props */}
-              <FormField
-                /* control takes the control of the login form we defined using useForm. */
-                control={LoginForm.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="example@email.com"
-                        type="email"
-                        {...field}
-                      />
-                    </FormControl>
-                    {/* Add a description for the form item for accessibility. */}
-                    <FormDescription>
-                      Your email will be your username
-                    </FormDescription>
-                    {/* Form messages, e.g. validation errors, required texts, etc., will go here. */}
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* Each FormField will have atleast the control, name and render props */}
-              <FormField
-                control={LoginForm.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input placeholder="" type="password" {...field} />
-                    </FormControl>
-                    {/* Add a description for the form item for accessibility. */}
-                    <FormDescription>
-                      Password must be at least 8 characters long.
-                    </FormDescription>
-                    {/* Form messages, e.g. validation errors, required texts, etc., will go here. */}
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* Sign In button. The submit handler passed on to the onSubmit as prop of the form tag will be triggered on clicking this button.*/}
-              <Button type="submit" className="my-4">Sign In</Button>
-            </form>
-          </Form>
-        </TabsContent>
-        <TabsContent value={UserLoginType.SIGN_UP} className="py-4">
-          {/* Use shadcn Form provider component. */}
-          {/* LoginForm is a useForm() object */}
-          <Form {...LoginForm}>
-            {/* Nest the HTML form tag inside the Form provider */}
-            {/* Pass the submit handler function to LoginForm.handleSubmit in the onSubmit prop.*/}
-            {/* LoginForm.handleSubmit will take care of event handling,  prevent default, etc. */}
-            <form
-              onSubmit={LoginForm.handleSubmit(handleUserSignUp)}
-              className=""
-            >
-              {/* Each FormField will have atleast the control, name and render props */}
-              <FormField
-                /* control takes the control of the login form we defined using useForm. */
-                control={LoginForm.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="example@email.com"
-                        type="email"
-                        {...field}
-                      />
-                    </FormControl>
-                    {/* Add a description for the form item for accessibility. */}
-                    <FormDescription>
-                      Your email will be your username
-                    </FormDescription>
-                    {/* Form messages, e.g. validation errors, required texts, etc., will go here. */}
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* Each FormField will have atleast the control, name and render props */}
-              <FormField
-                control={LoginForm.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input placeholder="" type="password" {...field} />
-                    </FormControl>
-                    {/* Add a description for the form item for accessibility. */}
-                    <FormDescription>
-                      Password must be at least 8 characters long.
-                    </FormDescription>
-                    {/* Form messages, e.g. validation errors, required texts, etc., will go here. */}
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* Sign In button. The submit handler passed on to the onSubmit as prop of the form tag will be triggered on clicking this button.*/}
-              <Button type="submit" className="my-4">Sign Up</Button>
-            </form>
-          </Form>
-        </TabsContent>
-      </Tabs>
+    <div className="flex flex-col flex-grow justify-center text-center items-center w-full min-h-screen ">
+      <div className="flex flex-col w-[400px] m-4">
+        <h1 className="p-2 text-left">Login with:</h1>
+        <div className="flex w-full p-2 justify-center gap-4">
+          <Button onClick={handleOAuthButtonClick}>Github</Button>
+          <Button onClick={handleOAuthButtonClick}>Google</Button>
+        </div>
+        <br />
+        <hr />
+      </div>
+      <div  className="w-[400px] m-4">
+        <h1 className="p-2 text-left">Or:</h1>
+        <Tabs defaultValue={UserLoginType.SIGN_IN}>
+          {/* List of tabs we have. Use grid to style the tab buttons properly.*/}
+          <TabsList className="grid w-full grid-cols-2">
+            {/* Trigger values to display the respective TabsContent. */}
+            <TabsTrigger value={UserLoginType.SIGN_IN}>Sign In</TabsTrigger>
+            <TabsTrigger value={UserLoginType.SIGN_UP}>Sign Up</TabsTrigger>
+          </TabsList>
+          <TabsContent value={UserLoginType.SIGN_IN} className="py-4">
+            {/* Use shadcn Form provider component. */}
+            {/* LoginForm is a useForm() object */}
+            <Form {...LoginForm}>
+              {/* Nest the HTML form tag inside the Form provider */}
+              {/* Pass the submit handler function to LoginForm.handleSubmit in the onSubmit prop.*/}
+              {/* LoginForm.handleSubmit will take care of event handling,  prevent default, etc. */}
+              <form
+                onSubmit={LoginForm.handleSubmit(handleUserSignIn)}
+                className=""
+              >
+                {/* Each FormField will have atleast the control, name and render props */}
+                <FormField
+                  /* control takes the control of the login form we defined using useForm. */
+                  control={LoginForm.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="example@email.com"
+                          type="email"
+                          {...field}
+                        />
+                      </FormControl>
+                      {/* Add a description for the form item for accessibility. */}
+                      <FormDescription>
+                        Your email will be your username
+                      </FormDescription>
+                      {/* Form messages, e.g. validation errors, required texts, etc., will go here. */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* Each FormField will have atleast the control, name and render props */}
+                <FormField
+                  control={LoginForm.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" type="password" {...field} />
+                      </FormControl>
+                      {/* Add a description for the form item for accessibility. */}
+                      <FormDescription>
+                        Password must be at least 8 characters long.
+                      </FormDescription>
+                      {/* Form messages, e.g. validation errors, required texts, etc., will go here. */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* Sign In button. The submit handler passed on to the onSubmit as prop of the form tag will be triggered on clicking this button.*/}
+                <Button type="submit" className="my-4">
+                  Sign In
+                </Button>
+              </form>
+            </Form>
+          </TabsContent>
+          <TabsContent value={UserLoginType.SIGN_UP} className="py-4">
+            {/* Use shadcn Form provider component. */}
+            {/* LoginForm is a useForm() object */}
+            <Form {...LoginForm}>
+              {/* Nest the HTML form tag inside the Form provider */}
+              {/* Pass the submit handler function to LoginForm.handleSubmit in the onSubmit prop.*/}
+              {/* LoginForm.handleSubmit will take care of event handling,  prevent default, etc. */}
+              <form
+                onSubmit={LoginForm.handleSubmit(handleUserSignUp)}
+                className=""
+              >
+                {/* Each FormField will have atleast the control, name and render props */}
+                <FormField
+                  /* control takes the control of the login form we defined using useForm. */
+                  control={LoginForm.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="example@email.com"
+                          type="email"
+                          {...field}
+                        />
+                      </FormControl>
+                      {/* Add a description for the form item for accessibility. */}
+                      <FormDescription>
+                        Your email will be your username
+                      </FormDescription>
+                      {/* Form messages, e.g. validation errors, required texts, etc., will go here. */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* Each FormField will have atleast the control, name and render props */}
+                <FormField
+                  control={LoginForm.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" type="password" {...field} />
+                      </FormControl>
+                      {/* Add a description for the form item for accessibility. */}
+                      <FormDescription>
+                        Password must be at least 8 characters long.
+                      </FormDescription>
+                      {/* Form messages, e.g. validation errors, required texts, etc., will go here. */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* Sign In button. The submit handler passed on to the onSubmit as prop of the form tag will be triggered on clicking this button.*/}
+                <Button type="submit" className="my-4">
+                  Sign Up
+                </Button>
+              </form>
+            </Form>
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 };
