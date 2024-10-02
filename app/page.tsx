f297719c8f9ea2aea68d01c3a43221160d2b9614
vikramsingh13 <vikramsingh.webdev@gmail.com>
2024-10-02 15:22:41 -0400

    login button moved to navbar and navbar structure slightly adjusted to better style the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -7,25 +7,11 @@ import { Button } from "@/components/ui/button";
 import { UserLoginType } from "@/types/types";
 
 export default function Home() {
-  // Initialize the router.
-  const router = useRouter();
-  // test user for development.
-  const userId = 'test-user';
-  // Function to handle the login button click by the user.
-  const handleLoginButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    // Get the login type from the button click event.
-    const loginType = event.currentTarget.getAttribute('loginType')
-    // Push the login page with the login type to the router.
-    router.push(`login/${loginType}`);
-  }
   return (
     <div className="">
-      { /* Redirect to /test-user/learning-section for now */ }
-      {/* { redirect(`/${userId}/dashboard`) } */}
+      hello 
 
-      {/* Takes the user to the signin/signup pages. */}
-      {/* This Login button redirects to the sign in page by using the loginType prop */}
-      <Button onClick={handleLoginButtonClick} loginType={UserLoginType.SIGN_IN}>Login</Button>
+      
     </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,8 +7,12 @@ import { useSession } from "next-auth/react";
 import { useRouter, redirect, usePathname } from "next/navigation";
 // Import the UserNotLoggedin component to delegate no user session found logic.
 import UserNotLoggedin from "@/components/user-not-loggedin";
+// Import shadcn ui components.
+import { Button } from "@/components/ui/button";
+// Import User login types for redirecting to the sign in/up page.
+import { UserLoginType } from "@/types/types";
 
-const Navbar = ({ children } : {children: React.ReactNode}) => {
+const Navbar = ({ children }: { children: React.ReactNode }) => {
   // Use the useSession consumer to manage sessions.
   // Destructure the data from useSession as session for better readability.
   const { data: session } = useSession();
@@ -18,13 +22,37 @@ const Navbar = ({ children } : {children: React.ReactNode}) => {
   // Update isSessionValid based on the changes to the session object.
   useEffect(() => {
     session ? setIsSessionValid(true) : setIsSessionValid(false);
-  }, [session])
+  }, [session]);
+
+  // Initialize the router.
+  const router = useRouter();
+  // Function to handle the login button click by the user.
+  const handleLoginButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    // Get the login type from the button click event.
+    const loginType = event.currentTarget.getAttribute("loginType");
+    // Push the login page with the login type to the router.
+    router.push(`login/${loginType}`);
+  };
 
   return (
-    <>
-      <navbar> Navbar code goes here </navbar>
-      { !isSessionValid ? <UserNotLoggedin /> : children }
-    </>
+    <div className="w-full">
+      <navbar className="flex bg-custom-bg-dark w-full p-4 justify-center text-center items-center">
+        <p>TutorFlex.app</p>
+        {/* Takes the user to the signin/signup pages. */}
+        {/* This Login button redirects to the sign in page by using the loginType prop */}
+        <div className="flex flex-grow justify-end">
+          <Button
+            onClick={handleLoginButtonClick}
+            loginType={UserLoginType.SIGN_IN}
+          >
+            Login
+          </Button>
+        </div>
+      </navbar>
+      {!isSessionValid ? <UserNotLoggedin /> : children}
+    </div>
   );
 };
 
