c7b4fbcef5df79528ce82303b57ab856531cedee
vikramsingh13 <vikramsingh.webdev@gmail.com>
2024-10-02 18:38:16 -0400

    dummy data added for testing dashboard and learning-section

---
+++ data/dummyCourseData.ts
@@ -0,0 +1,86 @@
+// Dummy course data will be used for use dashboard following the course creations logic flow, and will be used to test the learning section layout and functionality. 
+export const dummyCourseData = {
+  modules: {
+    1: {
+      title: "Introduction to Computers",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+      subModules: {
+        1: {
+          heading: "Sub Module 1",
+          content:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+        },
+        2: {
+          heading: "Sub Module 2",
+          content:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+        },
+        3: {
+          heading: "Sub Module 3",
+          content:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+        },
+        4: {
+          heading: "Sub Module 4",
+          content:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+        },
+      },
+    },
+    2: {
+      title: "Computer Hardware",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+      subModules: {
+        1: {
+          heading: "Sub Module 1",
+          content:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+        },
+        2: {
+          heading: "Sub Module 2",
+          content:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+        },
+        3: {
+          heading: "Sub Module 3",
+          content:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+        },
+        4: {
+          heading: "Sub Module 4",
+          content:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+        },
+      },
+    },
+    3: {
+      title: "Computer Software",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+      subModules: {
+        1: {
+          heading: "Sub Module 1",
+          content:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+        },
+        2: {
+          heading: "Sub Module 2",
+          content:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+        },
+        3: {
+          heading: "Sub Module 3",
+          content:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+        },
+        4: {
+          heading: "Sub Module 4",
+          content:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro illo, atque sit accusantium veritatis minima sunt vitae corrupti nemo est tenetur voluptatem recusandae consequuntur obcaecati libero aut corporis esse reprehenderit",
+        },
+      },
+    },
+  },
+};
