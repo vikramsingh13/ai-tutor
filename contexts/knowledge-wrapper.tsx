d9de6745ac69f30ccf3b5ba7cdfa5818a924afdc
vikramsingh13 <vikramsingh.webdev@gmail.com>
2024-10-07 18:56:12 -0400

    remove unused wrapper export code

--- contexts/knowledge-wrapper.tsx
+++ contexts/knowledge-wrapper.tsx
@@ -3,17 +3,15 @@ import { createContext } from "react";
 // Import the dummy course data to pass as the context.
 import { dummyCourseData } from "@/data/dummyCourseData";
 
+/* TODO
+// Helper function to update the current module index.
+const UpdateCourseDataCurrentModuleIndex = (moduleIndex: number) => {
+  dummyCourseData
+}
+*/
+
 // Takes the course data json with the modules and submodules.
 // This is the default course content.
 export const KnowledgeContext = createContext(dummyCourseData);
 
-/*
-// KnowledgeWrapper takes children of type ReactNode as a param.
-// Returns the children wrapped by the CourseContext context object.
-const KnowledgeWrapper = (children: React.ReactNode) => {
-  // Wrap the children with the CourseContext and return it.
-  return <CourseContext.Provider value={dummyCourseData}>{children}</CourseContext.Provider>;
-};
 
-export default KnowledgeWrapper;
-*/
