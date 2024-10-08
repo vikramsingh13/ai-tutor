8c8ef0ac98ac2cde12bdf8859c2cf89efa19a298
vikramsingh13 <vikramsingh.webdev@gmail.com>
2024-10-07 22:50:24 -0400

    updated the external ai address constants

--- lib/actions/external/ai-actions.ts
+++ lib/actions/external/ai-actions.ts
@@ -3,14 +3,16 @@
 // Fetch AI generated course data with a user input.
 // Takes a string param userInput and returns a JSON back.
 export const getAIGeneratedCourseData = async (userInput: string) => {
+  // Get the API root address from the environment.
+  const API_ROOT = process.env.NEXT_PUBLIC_AI_TUTOR_API_ADDRESS ? process.env.NEXT_PUBLIC_AI_TUTOR_API_ADDRESS : "http://localhost:8000";
   // API endpoint for getting course data from user input.
-  const API_ENDPOINT = `/ai-operations/course-generation/${userInput}`;
+  const API_ENDPOINT = `${API_ROOT}/ai-operations/course-generation/${userInput}`;
   // JSON data to be returned.
   let result = {};
   // Use try/catch before attempting to make the HTTP request.
   try {
     const data = await fetch(
-      `http://localhost:8000${API_ENDPOINT}`, {
+      `${API_ENDPOINT}`, {
         method: "GET",
       }
     );
