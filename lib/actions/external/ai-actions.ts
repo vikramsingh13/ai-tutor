a15cc6c1ea1cb38a1117e60309e4a07fa13ffaaa
vikramsingh13 <vikramsingh.webdev@gmail.com>
2024-10-10 20:30:22 -0400

    cleaned up the logic to get the external urls based on deployment environment

--- lib/actions/external/ai-actions.ts
+++ lib/actions/external/ai-actions.ts
@@ -1,22 +1,23 @@
 "use server";
 
+// Get the API root address from the environment.
+const API_ROOT =
+  process.env.DEPLOYMENT_ENVIRONMENT === "production"
+    ? process.env.NEXT_PUBLIC_AI_TUTOR_API_ADDRESS
+    : "http://localhost:8000";
+
 // Fetch AI generated course data with a user input.
 // Takes a string param userInput and returns a JSON back.
 export const getAIGeneratedCourseData = async (userInput: string) => {
-  // Get the API root address from the environment.
-  // const API_ROOT = process.env.NEXT_PUBLIC_AI_TUTOR_API_ADDRESS ? process.env.NEXT_PUBLIC_AI_TUTOR_API_ADDRESS : "http://localhost:8000";
-  const API_ROOT = "http://localhost:8000";
   // API endpoint for getting course data from user input.
   const API_ENDPOINT = `${API_ROOT}/ai-operations/course-generation/${userInput}`;
   // JSON data to be returned.
   let result = {};
   // Use try/catch before attempting to make the HTTP request.
   try {
-    const data = await fetch(
-      `${API_ENDPOINT}`, {
-        method: "GET",
-      }
-    );
+    const data = await fetch(`${API_ENDPOINT}`, {
+      method: "GET",
+    });
     result = await data.json();
     return result;
   } catch (e) {
@@ -29,21 +30,16 @@ export const getAIGeneratedCourseData = async (userInput: string) => {
 // Fetch AI Generated quiz data from module data.
 // Takes a JSON param of module information and returns a JSON with quiz questions and answers.
 export const getAIGeneratedQuizDataFromModule = async (moduleData: {}) => {
-  // Get the API root address from the environment.
-  const API_ROOT = process.env.NEXT_PUBLIC_AI_TUTOR_API_ADDRESS ? process.env.NEXT_PUBLIC_AI_TUTOR_API_ADDRESS : "http://localhost:8000";
-
   // API endpoint for getting quiz data from module data.
   const API_ENDPOINT = `${API_ROOT}/ai-operations/quiz-generation`;
   // JSON data to be returned.
   let result = {};
   // Use try/catch before attempting to make the HTTP request.
   try {
-    const data = await fetch(
-      `${API_ENDPOINT}`, {
-        method: "POST",
-        body: JSON.stringify(moduleData),
-      }
-    );
+    const data = await fetch(`${API_ENDPOINT}`, {
+      method: "POST",
+      body: JSON.stringify(moduleData),
+    });
     result = await data.json();
     return result;
   } catch (e) {
@@ -51,4 +47,4 @@ export const getAIGeneratedQuizDataFromModule = async (moduleData: {}) => {
     console.log((e as Error).message);
     return result;
   }
-}
+};
